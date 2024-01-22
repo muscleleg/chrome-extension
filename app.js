98e3fcdcaa5dcde7d5f1e0fdc32940c159d7c3d5
jaejunkim <aamoof@gmail.com>
2024-01-23 04:03:21 +0900

    재귀 삭제

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ chrome.storage.local.get(["github"]).then((result) => {
 });
 function setUsername(){
     const username = prompt("깃허브 ID:");
-    if(!username) setUsername();
     chrome.storage.local.set({github:username});
     document.getElementById("jandi").src = `https://ghchart.rshah.org/${username}`;
 }
